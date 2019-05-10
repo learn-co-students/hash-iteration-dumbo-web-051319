c01c2755d509311c15563811d45818fd7af1e1cf
Stefanos Ugbit <stefanosu422@gmail.com>
2019-05-10 05:51:25 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -11,4 +11,8 @@ def select_winner(passengers)
   passengers.each do |suite, name|
     if suite == :suite_a
       && name.start_with?("A")
-end
+        winner = name
+      end
+  end
+  
+  winner
